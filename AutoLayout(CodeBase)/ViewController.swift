c513e6b7925e4d2ca15644af3cc8574da6d928b8
Jamong-i <smkim961211@gmail.com>
2024-02-26 22:11:19 +0900

    자동레이아웃 하이랭커, 앵커를 사용하는 방법, 승수

--- AutoLayout(CodeBase)/ViewController.swift
+++ AutoLayout(CodeBase)/ViewController.swift
@@ -18,14 +18,21 @@ class ViewController: UIViewController {
         // autoresizingMask는 superview가 변함에 따라 subview의 크기를 어떻게 할 것인지에 대한 클래스로
         // autolayout에서 같이 사용된다면 충돌이 날 수 있어, 충돌 방지를 위해 Autoresizing을 사용하지 않는 것으로 선언
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Joun us today in our fun add games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        let attributedText = NSMutableAttributedString(string: "Joun us today in our fun add games!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
+//        textView.text = "Joun us today in our fun add games!"
+//        textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment = .center
         textView.isEditable = false
@@ -39,7 +46,7 @@ class ViewController: UIViewController {
         // imageView를 클로저로 만들어서 사용(위에)
 //        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first.png"))
 //        view.addSubview(imageView)
-        view.addSubview(bearImageView)
+//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
 //        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
@@ -49,14 +56,36 @@ class ViewController: UIViewController {
     }
     
     private func setupLayout() {
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        // 탑이미지컨테이너 뷰를 이용해서 세로일때 반 가로일때 반으로 나누는 것을 이용
+        let topImageContainerView = UIView()
+//        topImageContainerView.backgroundColor = .blue
+        view.addSubview(topImageContainerView)
+//        topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        //enable auto layout
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+//        topImageContainerView.leadingAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        
+//        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+//        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
